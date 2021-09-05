de86518ff273cd0252f288b24b0a5f5dbaf4c739
unknown <syful.cse.bd@gmail.com>
2021-09-05 09:30:12 +0600

    visitor log update2

--- app/Http/Controllers/FrontEnd/IndexController.php
+++ app/Http/Controllers/FrontEnd/IndexController.php
@@ -21,7 +21,8 @@ class IndexController extends Controller
 
         // get IP Address
         //$clientIP = $_SERVER['REMOTE_ADDR'];
-        $clientIP = $my_current_ip=exec("ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'");
+        $clientIP = $this->get_ip();
+        
         //$clientIP = '202.51.181.142';
         $geoip =  geoip()->getLocation($clientIP);
 
@@ -72,6 +73,28 @@ class IndexController extends Controller
         }
 
         return response()->json($clientIP, 200);
+    }
+
+
+    // Function to get the client ip address
+    public function get_ip()
+    {
+        $mainIp = '';
+        if (getenv('HTTP_CLIENT_IP'))
+            $mainIp = getenv('HTTP_CLIENT_IP');
+        else if (getenv('HTTP_X_FORWARDED_FOR'))
+            $mainIp = getenv('HTTP_X_FORWARDED_FOR');
+        else if (getenv('HTTP_X_FORWARDED'))
+            $mainIp = getenv('HTTP_X_FORWARDED');
+        else if (getenv('HTTP_FORWARDED_FOR'))
+            $mainIp = getenv('HTTP_FORWARDED_FOR');
+        else if (getenv('HTTP_FORWARDED'))
+            $mainIp = getenv('HTTP_FORWARDED');
+        else if (getenv('REMOTE_ADDR'))
+            $mainIp = getenv('REMOTE_ADDR');
+        else
+            $mainIp = 'UNKNOWN';
+        return $mainIp;
     }
 
 
