0ab7dee4a59aa67f7a82443c7ed94cf350ccf511
unknown <syful.cse.bd@gmail.com>
2021-09-16 17:28:18 +0600

    review in overall4

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,33 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', 'App\Http\Controllers\FrontEnd\IndexController@index');
 // Route::get('{slug}', 'App\Http\Controllers\FrontEnd\IndexController@index');
 
+// Artisan Start
+Route::namespace('App\Http\Controllers\Cmd')->prefix('cmd')->group( function () {
+        
+    Route::get('/', 'ArtisanController@index')->name('cmd.index');
+    Route::get('config-cache', 'ArtisanController@config')->name('cmd.config.cache');
+    Route::get('optimize', 'ArtisanController@optimize')->name('cmd.config.optimize');
+    //Clear Cache facade value:
+    Route::get('cache-clear', 'ArtisanController@cacheClear')->name('cmd.cache.clear');
+    //Route cache:
+    Route::get('route-cache', 'ArtisanController@routeCache')->name('cmd.route.cache');
+    //Clear Route cache:
+    Route::get('route-clear', 'ArtisanController@routeClear')->name('cmd.route.clear');
+    //Clear View cache:
+    Route::get('view-clear', 'ArtisanController@viewClear')->name('cmd.view.clear');
+    Route::get('clear', 'ArtisanController@clear')->name('cmd.clear');
+
+});
+// Artisan End
+
+
+
 Route::get('/admin/{any?}', 'App\Http\Controllers\Admin\AdminController@index')->where('any', '.*')->name('admin');
 
 Route::get('{any}', 'App\Http\Controllers\FrontEnd\IndexController@index')->where('any', '.*');
+
+
+
 
 
 
